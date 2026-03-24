d293efd8ca90595016e59e25a4b595fabd679b8b
Andreas Nazlidis <andreas221b@gmail.com>
2026-03-24 16:53:54 +0100

    add platform-specific main

--- src/main_windows.cpp
+++ src/main_windows.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-#ifdef _WIN32
+
 #include "windows.h"
-#endif  // _WIN32
 
 int main(int argc, const char** argv) {
-#ifdef _WIN32
   return WinMain(GetModuleHandle(nullptr), nullptr, GetCommandLineA(),
                  SW_SHOWDEFAULT);
-#endif  // _WIN32
 }
