11f96978f03eceebe55f4e6dfeea5b81580f0601
Andreas Nazlidis <andreas221b@gmail.com>
2026-03-24 20:35:57 +0100

    unify main impl for different platforms, make glad dependency external

--- include/application.h
+++ include/application.h
@@ -1,9 +1,9 @@
 #pragma once
 
+#include <glad/glad.h>
+
 #include <filesystem>
 #include <memory>
-
-#include "glad/glad.h"
 
 namespace core {
 class Application {
--- src/main_windows.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include "windows.h"
-
-int main(int argc, const char** argv) {
-  return WinMain(GetModuleHandle(nullptr), nullptr, GetCommandLineA(),
-                 SW_SHOWDEFAULT);
-}
